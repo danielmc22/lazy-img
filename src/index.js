b57ca1dda6ba3a19345319809030e54a6f2ec8bf
unknown <danielfm.c222@hotmail.com>
2024-01-04 00:09:09 -0500

    js advance

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const maximum = 122;
+const minimum = 1;
 
-console.log('Happy hacking :)')
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum //forma de sacar un # random entre un rango especifico en este caso para sacar una imagen random de un total de 120 imgs
+
+const createImageNode = () => {
+const container = document.createElement('div');
+container.className = 'p-4';
+  const imagen = document.createElement('img');
+  imagen.className = "mx-auto";
+  imagen.width = "270";
+  imagen.src = `https://randomfox.ca/images/${random()}.jpg`; // TODO CHANGE #
+  container.appendChild(imagen);
+
+  return container;
+};
+
+
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById('images');
+
+
+const btnAdd = document.getElementById('btnAdd');
+
+const addNewImg = () => {
+  const newImage = createImageNode()
+  mountNode.append(newImage)
+
+};
+btnAdd.addEventListener("click", addNewImg);
